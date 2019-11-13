50f98d95962448eb46322f558c4c6d085bd5edf3
unboundtech <it@dyadicsec.com>
2019-11-13 14:34:27 +0000

    Security fixes: EP moved from public subnet to private, security groups changed accordingly. CASP server: Java installation added. CASP_variables.tf file: user interface was correlated with format of UKC_variables.tf in EKM-Expredd-Deploy repository.

---
+++ casp-terraform/CASP_variables.tf
@@ -0,0 +1,35 @@
+variable "access_key" { default = "<Access Key>" }
+variable "secret_key" { default = "<Secret Key>" }
+variable "resource-group-name" { default = "Terraform-CASP-demo"}
+variable "ep_public_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "ep_private_key_path" { default = "/home/<your user>/.ssh/<private key>" }
+variable "partner_public_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "partner_private_key_path" { default = "/home/<your user>/.ssh/<private key>" }
+variable "key_name_0" { default = "Terraform-CASP-UKC-EP-key" }
+variable "key_name_1" { default = "Terraform-CASP-UKC-Partner-AUX-key" }
+variable "password1" { default = "Password1!" }
+variable "aws_region" { default = "<AWS region>" }
+variable "ukc_pac" { default = "ekm-<UKC version>-RHES.x86_64.rpm" }
+variable "os_user_0" { default = "centos" }
+variable "instance_type_ukc" { default = "<AWS instance type>" }
+
+variable "provide_ssh" {
+  description = "If true, id_rsa key will be copied to bastion and hosts in the private subnet will be accessible by 2 hops"
+}
+
+variable "casp_public_key_path" { default = "/home/<your user>/.ssh/<public key>" }
+variable "casp_private_key_path" { default = "/home/<your user>/.ssh/<private key>" }
+variable "key_name_3" { default = "Terraform-CASP-key" }
+variable "casp_backup_key_path" { default = "casp_backup.pem" }
+variable "casp_pac" { default = "casp-<CASP version>-RHES.x86_64.rpm" }
+variable "instance_type_casp" { default = "t3.medium" }
+
+variable "token-blockcypher-btc" { default = "replace it with blockcypher-btc token" }
+variable "token-blockcypher-btctest" { default = "replace it by blockcypher-btctest token" }
+
+variable "token-infura-eth" { default = "replace it with infura_eth token" }
+variable "token-infura-ethtest" { default = "replace it with infura_ethtest token" }
+
+#Firebase push notifaction push token
+variable "firebase_apikey" {default = "Place your firebase mobile  push token here" }
+
